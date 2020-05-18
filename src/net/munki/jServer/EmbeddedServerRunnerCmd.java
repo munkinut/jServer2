6570830d1732d8e96e6035920845be20843d33cd
Warren Milburn <warren@munki.net>
2020-05-18 12:17:29 +0100

    Renamed from EmbeddedServerRunner so EmbeddedServer can pickup classes and scripts by itself.

--- src/net/munki/jServer/EmbeddedServerRunnerCmd.java
+++ src/net/munki/jServer/EmbeddedServerRunnerCmd.java
@@ -9,21 +9,27 @@ package net.munki.jServer;
 /**
  * @author Warren Milburn
  */
-public class EmbeddedServerRunner {
+public class EmbeddedServerRunnerCmd {
 
     /**
      * Creates a new instance of TestEmbeddedServer
      */
-    public EmbeddedServerRunner() {
+    public EmbeddedServerRunnerCmd() {
     }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String[] services = {"net.munki.jServer.services.DefaultService",
-                                "net.munki.jServer.services.DateService",
-                                "net.munki.jServer.services.PingPongService"};
+        String[] services = new String[args.length];
+        String servicesLoc = "net.munki.jServer.services";
+        int index = 0;
+        for(String arg:args) {
+            String file = servicesLoc + "." + arg;
+            services[index] = file;
+            System.out.println(services[index]);
+            index++;
+        }
         EmbeddedServer es = new EmbeddedServer(12321, services);
         es.start();
     }
