15ff5f1e10770815dca4433fd4ba97d098a15ed3
Warren Milburn <warren@munki.net>
2020-05-21 05:26:07 +0100

    Gave the option of passing the port number on the command line.

--- src/net/munki/jServer/EmbeddedScriptServerRunner.java
+++ src/net/munki/jServer/EmbeddedScriptServerRunner.java
@@ -13,7 +13,7 @@ import net.munki.jServer.service.ScriptService;
  */
 public class EmbeddedScriptServerRunner {
 
-    private static String scriptsLocation;
+    private static final int DEFAULT_PORT = 12321;
     /**
      * Creates a new instance of TestEmbeddedServer
      */
@@ -24,8 +24,12 @@ public class EmbeddedScriptServerRunner {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        int port;
+        if ((args != null) && (args.length > 0))
+            port = Integer.parseInt(args[0]);
+        else port = DEFAULT_PORT;
         ScriptService service = new ScriptService();
-        EmbeddedScriptServer es = new EmbeddedScriptServer(12321, service);
+        EmbeddedScriptServer es = new EmbeddedScriptServer(port, service);
         es.start();
     }
 
