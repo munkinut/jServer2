906c67002d2a7acf77a51da768743aa984cfe3e4
Warren Milburn <warren@munki.net>
2020-05-21 05:29:18 +0100

    Added a Throwable constructor.

--- src/net/munki/jServer/exception/jServerException.java
+++ src/net/munki/jServer/exception/jServerException.java
@@ -2,6 +2,9 @@ package net.munki.jServer.exception;
 
 public class jServerException extends Throwable {
     public jServerException(String s) {
-
+        super(s);
+    }
+    public jServerException(Throwable t) {
+        super(t);
     }
 }
